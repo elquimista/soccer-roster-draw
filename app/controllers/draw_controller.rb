15880939808121b6368eb835d162aedd2a047ed2
el que m'est <14348482+elquimista@users.noreply.github.com>
2018-10-11 09:50:17 -0700

    Add action: draw#create

--- app/controllers/draw_controller.rb
+++ app/controllers/draw_controller.rb
@@ -2,4 +2,36 @@ class DrawController < ApplicationController
   def index
     @locations = Location.all
   end
+
+  def create
+    @match = nil
+    Match.transaction do
+      home_team = Team.create!
+      away_team = Team.create!
+      populate_rosters!(home_team, away_team)
+      @match = Match.create!(
+        location_id: params[:location_id],
+        date_time: date_time(params[:date_time]),
+        home_team: home_team,
+        away_team: away_team
+      )
+    end
+    unless @match&.persisted?
+      flash[:error] = ':( &nbsp;There was an error while trying to seed a draw.'
+      redirect_to action: :index
+    end
+  end
+
+  private
+
+  def date_time(obj)
+    DateTime.civil(obj[:year].to_i, obj[:month].to_i, obj[:day].to_i, obj[:hour].to_i, obj[:minute].to_i)
+  end
+
+  def populate_rosters!(home_team, away_team)
+    Player.all.shuffle.each.with_index do |player, i|
+      home_team.players << player if i.even?
+      away_team.players << player if i.odd?
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   get '/draw', to: 'draw#index'
+  post '/draw', to: 'draw#create'
 end
