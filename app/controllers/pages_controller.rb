e8894d9541f05513414817c9c94339ff2bd3693a
el que m'est <14348482+elquimista@users.noreply.github.com>
2018-10-11 11:35:54 -0700

    Show the last match in homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,10 @@
 class PagesController < ApplicationController
   def home
-    @match = Match.order(created_at: :desc).first
+    @match = Match
+      .includes(home_team: [:players], away_team: [:players])
+      .order(created_at: :desc)
+      .first
+    @home_team = @match.home_team
+    @away_team = @match.away_team
   end
 end
--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,8 @@ module SoccerRosterDraw
       g.orm :active_record, primary_key_type: :uuid
     end
 
+    config.active_record.default_timezone = :local
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
